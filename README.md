062535e7b95afc59451beb301ffafad3683cc513
Manpreet Rai <hi@manpreetrai.ca>
2023-12-25 19:58:35 +0530

    Max sub-array sum

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# Contains solutions to Leetcode Blind 75
+# My solutions to Leetcode Blind 75
+Leetcode Blind 75 is a list prepared by some individual, which contains some commonly occuring questions in technical interviews.
 
-Available at: [https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions](https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions)
+List Available here: [Leetcode Blind 75](https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions)
+
+# Completed
+### Arrays
+1. **[Two Sum](https://leetcode.com/problems/two-sum/)**
+2. **[Best Time to Buy and Sell Stock](https://leetcode.com/problems/best-time-to-buy-and-sell-stock/)**
+3. **[Contains Duplicate](https://leetcode.com/problems/contains-duplicate/)**
+4. **[Product of Array Except Self](https://leetcode.com/problems/product-of-array-except-self/)**
+5. **[Maximum Subarray](https://leetcode.com/problems/maximum-subarray/)**
+
+
+# Todo
+### Arrays
+6. **[Maximum Product Subarray](https://leetcode.com/problems/maximum-product-subarray/)**
+7. **[Find Minimum in Rotated Sorted Array](https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/)**
+8. **[Search in Rotated Sorted Array](https://leetcode.com/problems/search-in-rotated-sorted-array/)**
+9. **[3 Sum](https://leetcode.com/problems/3sum/)**
+10. **[Container With Most Water](https://leetcode.com/problems/container-with-most-water/)**
+
+
--- main.cpp
+++ main.cpp
@@ -6,14 +6,17 @@
 
 using namespace std;
 
+
 /**
  * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
  * You may assume that each input would have exactly one solution, and you may not use the same element twice.
  * You can return the answer in any order.
+ * Time complexity: O(N).
+ * Space complexity: O(N).
  * @see https://leetcode.com/problems/two-sum/
  * @param nums
  * @param target
- * @return vector<int>
+ * @return vector<int>: indices of numbers who's sum equals target.
  */
 vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int> m;
@@ -31,9 +34,11 @@ vector<int> twoSum(vector<int>& nums, int target) {
  * You are given an array prices where prices[i] is the price of a given stock on the ith day.
  * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
  * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+ * Time complexity: O(N).
+ * Space complexity: O(1).
  * @see https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
  * @param prices
- * @return int
+ * @return int: max profit.
  */
 int maxProfit(vector<int>& prices) {
     /**
@@ -67,15 +72,18 @@ int maxProfit(vector<int>& prices) {
     return max_profit;
 }
 
+
 /**
  * Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
  * @see https://leetcode.com/problems/contains-duplicate
  * @param nums
- * @return bool
+ * @return bool: true if duplicate exists/ false otherwise.
  */
 bool containsDuplicate(vector<int>& nums) {
     /**
      * Approach 1:
+     * Time complexity: O(N).
+     * Space complexity: O(N).
         unordered_set<int> s;
         for (auto num : nums) {
             if (const auto j = s.find(num); j != s.cend()) return true;
@@ -87,6 +95,8 @@ bool containsDuplicate(vector<int>& nums) {
 
     /**
      * Approach 2:
+     * Time complexity: O(N * Log N).
+     * Space complexity: O(1).
      */
     sort(nums.begin(), nums.end());
     for (int i = 0; i != nums.size() - 1; ++i) {
@@ -96,56 +106,99 @@ bool containsDuplicate(vector<int>& nums) {
 }
 
 
+/**
+ * Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ * Time complexity: O(N).
+ * Space complexity: O(N).
+ * @see https://leetcode.com/problems/product-of-array-except-self
+ * @param nums
+ * @return vector<int>: products of elements except self.
+ */
 vector<int> productExceptSelf(vector<int>& nums) {
-    vector<int> prefix (nums.size()), postfix (nums.size()), results (nums.size());
+    /** Approach 1:
+     */
+        int n = (int) nums.size();
+        vector<int> results (n, 1);
+        int pre = 1, post = nums[n-1];
 
-    for (int i = 0; i != nums.size(); ++i) {
-        if (i == 0) {
-            prefix.push_back(nums[i] * 1);
-        } else prefix.push_back(nums[i] * prefix[i-1]);
+        for (int i = 0; i != n; ++i) {
+            results[i] = pre;
+            pre *= nums[i];
+        }
 
-        cout << prefix[i] << " : ";
+        for (int i = n - 2; i != -1; --i) {
+            results[i] *= post;
+            post *= nums[i];
+        }
+
+        return results;
+
+    /**
+     * Approach 2:
+     *
+        vector<int> pre (nums.size()), post (nums.size()), results (nums.size());
+
+        for (int i = 0, j = nums.size() - 1; i != nums.size() && j != -1; ++i, --j) {
+            if (i == 0) pre[i] = nums[i];
+            else pre[i] = nums[i] * pre[i-1];
+
+            if (j == nums.size() - 1) post[j] = nums[j];
+            else post[j] = nums[j] * post[j+1];
+        }
+
+        for (int i = 0; i != nums.size(); ++i) {
+            if (i == 0) results[i] = post[i+1];
+            else if (i == nums.size() - 1) results[i] = pre[i-1];
+            else results[i] = pre[i-1] * post[i+1];
+        }
+
+        return results;
+     */
+}
+
+
+/**
+ * Given an integer array nums, find the subarray with the largest sum, and return its sum.
+ * Time complexity: O(N).
+ * Space complexity: O(1).
+ * @param nums
+ * @return int: max sub-array sum.
+ */
+int maxSubArray(vector<int>& nums) {
+    int max_sum = nums[0], curr_sum = nums[0];
+
+    for (int i = 1; i != nums.size(); ++i) {
+        if (curr_sum < 0) curr_sum = 0;
+        curr_sum += nums[i];
+        max_sum = max(max_sum, curr_sum);
     }
 
-    for (int i = nums.size() - 1; i != -1; --i) {
-        if (i == nums.size() - 1 ) {
-            postfix.insert(postfix.begin(), nums[i] * 1);
-        }
-        postfix.insert(postfix.begin(), nums[i] * postfix[i+1]);
-    }
-
-    for (int i = 0; i != nums.size(); ++i) {
-        if (i == 0) {
-            results.push_back(postfix[i+1] * 1);
-        }
-        else if (i == nums.size() - 1 ) {
-            results.push_back(prefix[i-1] * 1);
-        }
-        else {
-            results.push_back(prefix[i-1] * postfix[i+1]);
-        }
-    }
-
-    return results;
+    return max_sum;
 }
 
 
 int main() {
-    vector<int> nums {2,7,011,15};
+    vector<int> nums {2,7,11,15};
     int target = 9;
-
     auto results = twoSum(nums, target);
-    cout << results[0] << ':' << results[1] << '\n';
+    cout << "Two sum: " << results[0] << ':' << results[1] << '\n';
 
     vector<int> prices {7,1,5,3,6,4};
     cout << "Max profit: " << maxProfit(prices) << '\n';
 
     vector<int> duplicateNums {1,2,3,1};
-    cout << "Contains duplicate: 0- false, 1- true: " << containsDuplicate(duplicateNums) << '\n';
+    cout << "Contains duplicate: " << containsDuplicate(duplicateNums) << '\n';
 
-    vector selfNums {1,2,3,4};
+    vector<int> selfNums {1,2,3,4};
     vector<int> resultsSelf = productExceptSelf(selfNums);
-    cout << resultsSelf[0] << resultsSelf[1];
+    cout << "Product except self: ";
+    for(int i : resultsSelf) cout << i << ' ';
+    cout << '\n';
+
+    vector<int> subArrayNums {-2,1,-3,4,-1,2,1,-5,4};
+    cout << "Max sub-array sum: " << maxSubArray(subArrayNums) << '\n';
 
     return 0;
 }
